7bb9a53539aff1af46744b9eafa3fef82e55bbf0
PolazhinetsA <32923196+PolazhinetsA@users.noreply.github.com>
2019-09-12 20:20:28 +0300

    Add files via upload

--- server.c
+++ server.c
@@ -116,3 +116,13 @@ void job(int fd_cli)
     unlink(fnam_in );
     unlink(fnam_out);
 }
+
+void recvfile(int dst, int src, int nbytes)
+{
+    char buf[0x1000];
+
+    while (nbytes > sizeof(buf))
+        nbytes -= write(dst, buf, read(src, buf, sizeof(buf)));
+    while (nbytes > 0)
+        nbytes -= write(dst, buf, read(src, buf, nbytes));
+}
